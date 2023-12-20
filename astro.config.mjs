7007d9f00bbc6f4c9f51a6e70f597875ba8e6ce5
Daison <daisonthms@gmail.com>
2023-12-20 18:33:12 +0530

    setup vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
-  integrations: [sitemap(), robotsTxt({
-    sitemap: ["https://gianmarco.xyz/sitemap-index.xml", "https://gianmarco.xyz/sitemap-0.xml"]
-  }), solidJs(), UnoCSS({
-    injectReset: true
-  })],
-  markdown: {
-    remarkPlugins: [remarkReadingTime]
-  },
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel()
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'PressStart2P': '"Press Start 2P"',
+        'FiraCode': 'Fira Code',
+      },
+      animation: {
+        'typing': 'typing 1.7s steps(10, end) forwards, blink 1.5s 3s step-end infinite',
+      },
+      keyframes: {
+        typing: {
+          "0%": { width: "0" },
+          "100%": { width: "100%" },
+        },
+        blink: {
+          "0%": { 'border-color': 'rgba(255, 0, 0, 0.2)'  },
+          "50%": { 'border-color': 'red'  },
+        }
+      },
+    },
+  },
+  plugins: [],
+}
