c87e0339539bf735b7546d6ed7f499999492153e
Daison <daisonthms@gmail.com>
2023-12-20 18:29:59 +0530

    setup vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,22 @@ import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gianmarco.xyz/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-  ],
+  integrations: [sitemap(), robotsTxt({
+    sitemap: ["https://gianmarco.xyz/sitemap-index.xml", "https://gianmarco.xyz/sitemap-0.xml"]
+  }), solidJs(), UnoCSS({
+    injectReset: true
+  })],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   output: "server",
-  adapter: netlify(),
+  adapter: vercel()
 });
